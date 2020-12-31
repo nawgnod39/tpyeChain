99014023b273afb6816186c8bd20122b1de79168
takdongwan <dongwantak@gmail.com>
2020-12-31 14:13:00 +0900

    classes on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {//생성자는 메소드인데 클래스가 시작할 때마다 호출 됨.
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "tak",
-  age: 28,
-  gender: "male"
-};
+
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${
     person.gender
   }!`;
 };
-  
-console.log(sayHi(person));
- export {};
+
+console.log(sayHi(lynn));
+
+export {};
