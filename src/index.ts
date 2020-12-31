04ea091364a5dd8bbd11bfc997589c4033b0e0d9
takdongwan <dongwantak@gmail.com>
2020-12-31 13:57:46 +0900

    인터페이스

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
-  };
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "tak",
+  age: 28,
+  gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }!`;
+};
   
-  console.log(sayHi("tak", 28, "male"));
-  
-  export {};
+console.log(sayHi(person));
+ export {};
